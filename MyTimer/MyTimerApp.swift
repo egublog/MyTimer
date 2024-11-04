40385a2b7815b81266f27ffe6d647fbe8072ad5a
egublog <egukei0729@gmail.com>
2024-11-04 11:06:59 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by 江口敬太郎 on 2024/11/04.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
